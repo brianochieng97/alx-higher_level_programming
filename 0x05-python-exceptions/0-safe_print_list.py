bf9f2429a06328d6da71bb728f75acbb5d5f7db0
brianochieng97 <brianochieng9725@gmail.com>
2022-10-11 13:53:58 -0700

    Add solution to the tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    int = my_list
     try:
-        my_list = int[("%d "; x])
-        print(my_list)
-    except valueError:
-        print("x is not an integer")
-
+        num = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            num += 1
+            for x in range(num):
+                print("", end="")
+        print()
+        return num
+    except IndexError:
+        print()
+        return num
